0ee1f8401239e641c85cfa159632ad2c9cd2a324
Ecsedi Milan <milan200314@gmail.com>
2022-10-30 16:07:38 +0100

    Bank osztály metodusai létrefozva

--- src/hu/petrik/bankiszolgaltatasok/Bank.java
+++ src/hu/petrik/bankiszolgaltatasok/Bank.java
@@ -1,4 +1,42 @@
 package hu.petrik.bankiszolgaltatasok;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bank {
+
+    private List<Szamla> szamlaLista;
+
+
+    public Bank(){
+
+
+
+
+    }
+    public Szamla SzamlaNyitas(Tulajdonos tulajdonos, int hitelKeret)
+    {
+            szamlaLista= new List<Szamla>();
+
+
+
+
+    }
+
+    public int getOsszEgyenleg(Tulajdonos tulajdonos)
+    {
+
+        return
+    }
+    public Szamla getLegnagyobbEgyenleguSzamla(Tulajdonos tulajdonos){
+
+    }
+
+    public long getOsszHitelkeret(){
+
+    }
+
+
+
+
 }
